f2f8a35ed234f8afa92899491a7e18607d170c9a
bread <ismatch@163.com>
2022-06-08 23:18:51 +0800

    抽象工厂模式

---
+++ EDC.DesignPattern.AbstractFactory/ConcreteFactory/BreadSkinFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDC.DesignPattern.AbstractFactory
+{
+    public class BreadSkinFactory : ISkinFactory
+    {
+        public IButton CreateButton()
+        {
+            return new BreadButton();
+        }
+
+        public IComboBox CreateComboBox()
+        {
+            return new BreadComboBox();
+        }
+
+        public ITextField CreateTextField()
+        {
+            return new BreadTextField();
+        }
+    }
+}
---
+++ EDC.DesignPattern.AbstractFactory/ConcreteProduct/BreadTextField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDC.DesignPattern.AbstractFactory
+{
+    public class BreadTextField : ITextField
+    {
+        public void Display()
+        {
+            Console.WriteLine("显示面包边框文本框...");
+        }
+    }
+}
