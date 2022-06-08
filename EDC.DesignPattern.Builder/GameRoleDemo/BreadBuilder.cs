590488487fda85f771ec0e043ab5522a137a7406
bread <ismatch@163.com>
2022-06-08 23:53:47 +0800

    建造者模式

---
+++ EDC.DesignPattern.Builder/GameRoleDemo/BreadBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDC.DesignPattern.Builder
+{
+    /// <summary>
+    /// 面包建造器 ： 具体建造者
+    /// </summary>
+    public class BreadBuilder : ActorBuilder
+    {
+        public override void BuildCostume()
+        {
+            actor.Costume = "一个有个性的盔甲";
+        }
+
+        public override void BuildFace()
+        {
+            actor.Face = "靓仔";
+        }
+
+        public override void BuildHairStyle()
+        {
+            actor.HairStyle = "爆炸头";
+        }
+
+        public override void BuildSex()
+        {
+            actor.Sex = "男";
+        }
+
+        public override void BuildType()
+        {
+            actor.Type = "面包";
+        }
+    }
+}
